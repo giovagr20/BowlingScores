f221e3d4c74972a0a97850d141b8edc0b1120801
Giovanni Gomez <giovanni.gomez@unosquare.com>
2024-03-26 17:40:02 -0500

    It was added spare test

--- src/test/java/com/adurolife/exercise/services/BowlingGameServiceTest.java
+++ src/test/java/com/adurolife/exercise/services/BowlingGameServiceTest.java
@@ -51,8 +51,55 @@ class BowlingGameServiceTest {
     }
 
     @Test
-    void givenVariousPins_whenUserChooseReset_thenResetGame() {
+    void givenASpare_whenIsSecondFrameAndFirstRoll_thenPlus10() {
+        final int PINS_FIRST_TRY = 3;
+        final int PINS_SECOND_TRY = 7;
+        final int EXPECTED_RESULT = 20;
 
+        bowlingGameService.roll(PINS_FIRST_TRY);
+        bowlingGameService.roll(PINS_SECOND_TRY);
+
+        assertEquals(EXPECTED_RESULT, bowlingGameService.getScore());
+        assertEquals(2, bowlingGameService.getCurrentFrame());
+    }
+
+    @Test
+    void givenASpare_whenIsSecondFrameAndSecondRoll_thenPlus10() {
+        final int PINS_FIRST_TRY = 1;
+        final int PINS_SECOND_TRY = 7;
+        final int PINS_THIRD_TRY = 3;
+        final int EXPECTED_RESULT = 21;
+
+        bowlingGameService.roll(PINS_FIRST_TRY);
+        bowlingGameService.roll(PINS_SECOND_TRY);
+        bowlingGameService.roll(PINS_THIRD_TRY);
+
+        assertEquals(EXPECTED_RESULT, bowlingGameService.getScore());
+        assertEquals(2, bowlingGameService.getCurrentFrame());
+        assertEquals(2, bowlingGameService.getCurrentRoll());
+    }
+
+    @Test
+    void givenASpare_whenIsThirdFrameAndFirstRoll_thenPlus10() {
+        final int PINS_FIRST_TRY = 1;
+        final int PINS_SECOND_TRY = 7;
+        final int PINS_THIRD_TRY = 4;
+        final int PINS_FOURTH_TRY = 6;
+        final int EXPECTED_RESULT = 28;
+
+        bowlingGameService.roll(PINS_FIRST_TRY);
+        bowlingGameService.roll(PINS_SECOND_TRY);
+        bowlingGameService.roll(PINS_THIRD_TRY);
+        bowlingGameService.roll(PINS_FOURTH_TRY);
+
+        assertEquals(EXPECTED_RESULT, bowlingGameService.getScore());
+        assertEquals(3, bowlingGameService.getCurrentFrame());
+        assertEquals(1, bowlingGameService.getCurrentRoll());
+    }
+
+
+    @Test
+    void givenVariousPins_whenUserChooseReset_thenResetGame() {
         final int PINS_FIRST_TRY = 5;
         final int PINS_SECOND_TRY = 10;
         final int EXPECTED_RESULT_SCORE = 0;
